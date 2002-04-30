3735dc9732fd9d7f6f8b125a0bd22521dd21d87f
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2002-04-30 15:07:04 +0000

    Add support for multiple roots.

--- camfr/math/calculus/croot/mueller.h
+++ camfr/math/calculus/croot/mueller.h
@@ -20,16 +20,43 @@
 //
 // mueller
 //
-//   Uses the mueller algorithm to find a complex root of f based on initial
-//   estimates a and b.
+//   Uses the mueller algorithm to find a complex root of f based on 
+//   1, 2 or 3 initial estimates.
 //   prev_zeros can contain a list of known zeros of f, which will then be
 //   deflated.
 //
 /////////////////////////////////////////////////////////////////////////////
 
+Complex mueller(ComplexFunction& f, const Complex& a,
+                Real eps=1e-14, const std::vector<Complex>* prev_zeros=0,
+                int maxiter=100, bool *errorptr=0, bool verbose=false);
+
 Complex mueller(ComplexFunction& f, const Complex& a, const Complex& b,
                 Real eps=1e-14, const std::vector<Complex>* prev_zeros=0,
                 int maxiter=100, bool *errorptr=0, bool verbose=false);
+
+Complex mueller(ComplexFunction& f, const Complex& a, const Complex& b,
+                const Complex& c,
+                Real eps=1e-14, const std::vector<Complex>* prev_zeros=0,
+                int maxiter=100, bool *errorptr=0, bool verbose=false);
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+// mueller_multiple
+//
+//  Given an inital zero z0, locate additional zeros at or around z0.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+std::vector<Complex> mueller_multiple
+  (ComplexFunction& f, const Complex& z0, Real eps=1e-14, int maxiter=50);
+
+std::vector<Complex> mueller_multiple
+  (ComplexFunction& f, const std::vector<Complex>& z0, Real eps=1e-14, 
+   int maxiter=50);
+
 
 
 
