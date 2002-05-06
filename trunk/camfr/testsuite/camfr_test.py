e23d6ef333aef3717dbfcef59a79e250eb72f9d8
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2002-05-06 17:08:54 +0000

    New test.

--- trunk/camfr/testsuite/camfr_test.py
+++ trunk/camfr/testsuite/camfr_test.py
@@ -9,12 +9,13 @@
 import unittest
 
 import blazed_grating, substacks, planarTE, planarTM, VCSEL, SpE, \
-       PhC_splitter, metal_splitter, expressions, cladding, grating
+       PhC_splitter, metal_splitter, expressions, cladding, grating, \
+       degenerate
 
 alltests = unittest.TestSuite((blazed_grating.suite, substacks.suite, 
        planarTE.suite, planarTM.suite, VCSEL.suite, SpE.suite,
        PhC_splitter.suite, metal_splitter.suite, expressions.suite,
-       cladding.suite, grating.suite))
+       cladding.suite, grating.suite, degenerate.suite))
 
 if __name__ == "__main__":
     r = unittest.TextTestRunner()
