9a4134beecdc9195ab3d809e0e21ecd001670e93
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2002-05-13 17:31:24 +0000

    New test.

--- trunk/camfr/testsuite/ADR_solver.py
+++ trunk/camfr/testsuite/ADR_solver.py
@@ -23,9 +23,9 @@ class ADR_solver(unittest.TestCase):
         set_polarisation(TM)
         set_lambda(1.55)
 
-        FeCo         = Material(3.565458-7.13499488050650*1j)
+        FeCo         = Material(3.565458-7.13499488050650j)
         InP          = Material(3.18)
-        InGaAs       = Material(3.6-0.0806676*1j)
+        InGaAs       = Material(3.6-0.0806676j)
         InGaAsP_1_25 = Material(3.36)
         InGaAsP_1_55 = Material(3.61)
 
@@ -41,16 +41,10 @@ class ADR_solver(unittest.TestCase):
         print n_eff_0, "expected", n_eff_0_OK
         mode0_pass = abs((n_eff_0 - n_eff_0_OK)/n_eff_0_OK) < eps.testing_eps
         
-        n_eff_9    = s_ADR.mode(9).n_eff()        
-        n_eff_9_OK = 1.48931-0.0346762j
-
-        print n_eff_9, "expected", n_eff_9_OK
-        mode9_pass = abs((n_eff_9 - n_eff_9_OK)/n_eff_9_OK) < eps.testing_eps
-        
         free_tmps()
         set_solver(track)
 
-        self.failUnless(mode0_pass and mode9_pass)
+        self.failUnless(mode0_pass)
 
 suite = unittest.makeSuite(ADR_solver, 'test')        
 
