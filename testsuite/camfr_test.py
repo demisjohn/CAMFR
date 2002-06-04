7b4eca85cad2793ae83300083b6fe7f143bb3500
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2002-06-04 18:11:29 +0000

    New tests.

--- testsuite/camfr_test.py
+++ testsuite/camfr_test.py
@@ -11,14 +11,14 @@ import unittest
 import blazed_grating, substacks, planarTE, planarTM, VCSEL, SpE, \
        PhC_splitter, metal_splitter, expressions, cladding, grating, \
        degenerate, ADR_solver, precision, infstack, taper, rods, \
-       TEM_field
+       TEM_field, grating2, polariton, gaussian
 
 alltests = unittest.TestSuite((blazed_grating.suite, substacks.suite, 
        planarTE.suite, planarTM.suite, VCSEL.suite, SpE.suite,
        PhC_splitter.suite, metal_splitter.suite, expressions.suite,
        cladding.suite, grating.suite, degenerate.suite, ADR_solver.suite,
        precision.suite, infstack.suite, taper.suite, rods.suite,
-       TEM_field.suite))
+       TEM_field.suite, grating2.suite, polariton.suite, gaussian.suite))
 
 if __name__ == "__main__":
     r = unittest.TextTestRunner()
---
+++ testsuite/polariton.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+
+#############################################################################
+#
+# Polaritonic material.
+#
+#############################################################################
+
+from camfr import *
+from cmath import *
+import unittest, eps
+
+class polariton(unittest.TestCase):
+    def testpolariton(self):
+
+        """Polariton 2"""
+
+        print
+        print "Running polariton..."
+
+        set_N(40)
+        set_polarisation(TM)
+
+        # Geometry parameters.
+
+        a = .600        # period
+        r = a*.250/2.0  # rods radius
+
+        f = 0.6
+        set_lambda(a/f)
+
+        # Materials.
+
+        air = Material(1.0)
+
+        epsinf = 5.1
+        fL = 1.0
+        fT = 0.4
+
+        eps_r = epsinf*(f*f-fL*fL)/(f*f-fT*fT)
+        polmat = Material(sqrt(eps_r))
+
+        # Calculate band diagram
+
+        slab1 = Slab(air(0.5*a - r) + polmat(r))
+        slab2 = Slab(air(0.5*a))
+
+        s = BlochStack(slab1(2*r) + slab2(a-2*r))
+        s.calc()
+
+        kz = s.mode(0).kz()
+        kz_OK = 3.80047194128-3.70167318519e-05j
+        
+        print kz, "expected", kz_OK
+        kz_pass = abs((kz - kz_OK) / kz_OK) < eps.testing_eps
+
+        free_tmps()
+        set_polarisation(TE)
+           
+        self.failUnless(kz_pass)
+
+suite = unittest.makeSuite(polariton, 'test')
+
+if __name__ == "__main__":
+    unittest.main()
