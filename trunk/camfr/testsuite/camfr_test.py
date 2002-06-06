0a428bf82bd1524af029f0b9835c7a559eeb0632
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2002-06-06 18:21:35 +0000

    New test.

--- trunk/camfr/testsuite/camfr_test.py
+++ trunk/camfr/testsuite/camfr_test.py
@@ -11,14 +11,15 @@ import unittest
 import blazed_grating, substacks, planarTE, planarTM, VCSEL, SpE, \
        PhC_splitter, metal_splitter, expressions, cladding, grating, \
        degenerate, ADR_solver, precision, infstack, taper, rods, \
-       TEM_field, grating2, polariton, gaussian, wg
+       TEM_field, grating2, polariton, gaussian, wg, dent
 
 alltests = unittest.TestSuite((blazed_grating.suite, substacks.suite, 
        planarTE.suite, planarTM.suite, VCSEL.suite, SpE.suite,
        PhC_splitter.suite, metal_splitter.suite, expressions.suite,
        cladding.suite, grating.suite, degenerate.suite, ADR_solver.suite,
        precision.suite, infstack.suite, taper.suite, rods.suite, wg.suite,
-       TEM_field.suite, grating2.suite, polariton.suite, gaussian.suite))
+       TEM_field.suite, grating2.suite, polariton.suite, gaussian.suite,
+       dent.suite))
 
 if __name__ == "__main__":
     r = unittest.TextTestRunner()
