66ce23901ab4cc42e4107e15e41222b502718a50
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2002-06-07 21:03:51 +0000

    New way of plotting fields.

--- visualisation/camfr_tk.py
+++ visualisation/camfr_tk.py
@@ -24,51 +24,17 @@ def plot_neff(waveguide):
 	v.append((n.real, n.imag))
     create_window_and_draw(PolyMarker(v, fillcolor='white'))
 
-def plot_E1(wg, mode, PML=0):
+def plot_field(waveguide, mode, function, steps=100):
     v = []
-    for i in range(100):
-	x = i * wg.width().real / 100.0
-	v.append((x, abs(wg.mode(mode).field(Coord(x-PML*1j, 0, 0)).E1())))
+    PML = waveguide.get_imag_start_thickness()
+    for i in range(steps):
+	x = i * waveguide.width().real / (1.0 * steps)
+	v.append((x,function(waveguide.mode(mode).field(Coord(x+PML*1j,0,0)))))
     plot_vector(v)
     
-def plot_E2(wg, mode, PML=0):
+def plot_n(waveguide, steps=100):
     v = []
-    for i in range(100):
-	x = i * wg.width().real / 100.0
-	v.append((x, abs(wg.mode(mode).field(Coord(x-PML*1j, 0, 0)).E2())))
-    plot_vector(v)
-
-def plot_Ez(wg, mode, PML=0):
-    v = []
-    for i in range(100):
-	x = i * wg.width().real / 100.0
-	v.append((x, abs(wg.mode(mode).field(Coord(x-PML*1j, 0, 0)).Ez())))
-    plot_vector(v)
-
-def plot_H1(wg, mode, PML=0):
-    v = []
-    for i in range(100):
-	x = i * wg.width().real / 100.0
-	v.append((x, abs(wg.mode(mode).field(Coord(x-PML*1j, 0, 0)).H1())))
-    plot_vector(v)
-    
-def plot_H2(wg, mode, PML=0):
-    v = []
-    for i in range(100):
-	x = i * wg.width().real / 100.0
-	v.append((x, abs(wg.mode(mode).field(Coord(x-PML*1j, 0, 0)).H2())))
-    plot_vector(v)
-
-def plot_Hz(wg, mode, PML=0):
-    v = []
-    for i in range(100):
-	x = i * wg.width().real / 100.0
-	v.append((x, abs(wg.mode(mode).field(Coord(x-PML*1j, 0, 0)).Hz())))
-    plot_vector(v)
-
-def plot_n(waveguide):
-    v = []
-    for i in range(100):
-	x = i * waveguide.width().real / 100.0
-	v.append((x,abs(waveguide.n(Coord(x, 0, 0)))))
+    for i in range(steps):
+	x = i * waveguide.width().real / (1.0*steps)
+	v.append((x, abs(waveguide.n(Coord(x, 0, 0)))))
     plot_vector(v)
