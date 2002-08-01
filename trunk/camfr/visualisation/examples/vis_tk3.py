13c93c98294525b99d57fd95d3e161e95b441f1e
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2002-08-01 18:54:56 +0000

    Update.

---
+++ trunk/camfr/visualisation/examples/vis_tk3.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+
+####################################################################
+#
+# Illustrates 2D plotting from Tk.
+#
+####################################################################
+
+from camfr import *
+from Numeric import *
+from camfr_tk import *
+
+set_N(40)
+set_lambda(1.55)
+
+# Define materials.
+
+GaAs = Material(3.5)
+air  = Material(1.0)
+
+# Define geometry.
+
+set_lower_PML(-0.1)
+set_upper_PML(-0.1)
+
+wg  = Slab(air(2.0) + GaAs(0.2) + air(2.0))
+gap = Slab(air(4.2))
+
+s = Stack(wg(0) + gap(1) + wg(0))
+
+# Do some plotting.
+
+r_x = arange( 1.5, 2.8, 0.05)
+r_z = arange(-1.0, 2.0, 0.05)
+
+plot_n(s, r_x, r_z)
+
+inc = zeros(N())
+inc[0] = 1
+s.set_inc_field(inc)
+
+plot_field(s, lambda f : f.E2().real, r_x, r_z)
+
+animate_field(s, lambda f : f.E2(), r_x, r_z)
+
