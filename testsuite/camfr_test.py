f369905cdb5e1df9ab958a5e140da49fbbdc755d
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2003-01-31 15:41:02 +0000

    New test.

--- testsuite/camfr_test.py
+++ testsuite/camfr_test.py
@@ -11,12 +11,12 @@ import unittest
 import blazed_grating, substacks, planarTE, planarTM, VCSEL, SpE, \
        PhC_splitter, expressions, cladding, grating, coupled, \
        degenerate, precision, infstack, taper, rods, \
-       TEM_field, grating2, polariton, gaussian, wg, dent
+       TEM_field, grating2, polariton, gaussian, wg, dent, stack0
 
 alltests = unittest.TestSuite((blazed_grating.suite, substacks.suite, 
        planarTE.suite, planarTM.suite, VCSEL.suite, SpE.suite,
        PhC_splitter.suite, expressions.suite, coupled.suite,
-       cladding.suite, grating.suite, degenerate.suite,
+       cladding.suite, grating.suite, degenerate.suite, stack0.suite,
        precision.suite, infstack.suite, taper.suite, rods.suite, wg.suite,
        TEM_field.suite, grating2.suite, polariton.suite, gaussian.suite,
        dent.suite))
---
+++ testsuite/stack0.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python
+
+####################################################################
+#
+# stack with zero length first slab
+#
+####################################################################
+
+from camfr_work import *
+
+import unittest, eps
+
+class stack0(unittest.TestCase):
+    def testSpE(self):
+        
+        """Stack0"""
+
+        print
+        print "Running stack0..."
+
+        set_lambda(1.55)
+        set_N(5)
+        set_polarisation(TE)
+        
+        AlGaAs_m = Material(3.34)
+        air_m    = Material(1)
+
+        AlGaAs = Slab(AlGaAs_m(1))
+        air    = Slab(air_m(1))
+        
+        s = Stack(AlGaAs(0) + air(1) + AlGaAs(0))
+
+        inc = zeros(N())
+        inc[0] = 1
+        s.set_inc_field(inc)
+
+        s.calc()
+        
+        E = s.field(Coord(0.5,0,0.5)).E2()
+        E_OK =  37.3792383401-24.3729597789j
+        
+        print E, "expected", E_OK
+        
+        E_pass = abs((E - E_OK) / E_OK) < eps.testing_eps
+
+        free_tmps()
+        
+        self.failUnless(E_pass)
+
+suite = unittest.makeSuite(stack0, 'test')        
+
+if __name__ == "__main__":
+    unittest.main()
+    
+
+
