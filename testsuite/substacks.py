918ef30dfbf76244a5b72a8b8a6de97e010ac063
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2003-03-06 13:07:25 +0000

    Better test.

--- testsuite/substacks.py
+++ testsuite/substacks.py
@@ -21,25 +21,28 @@ class substacks(unittest.TestCase):
         set_lambda(1.1)
         set_N(50)
 
+        set_lower_PML(-0.001)
+        set_upper_PML(-0.001)
+
         a = Material(1)
         b = Material(2)
 
-        e1 = Expression(a(1.0) + b(0.5))
-        e2 = Expression(a(0.5) + b(1.0))
+        e1 = Expression(a(0.6) + b(0.5))
+        e2 = Expression(a(0.5) + b(0.6))
 
-        s0 = Slab(a(1) + e1 + e2 + a(1))
+        s0 = Slab(a(0.2) + e1 + e2 + a(1.2))
 
-        s1 = Slab(e1 + 2*e2 + a(0.5))
+        s1 = Slab(a(0.1) + e1 + 2*e2 + a(0.2))
 
         e3 = Expression(s0(1) + s1(2))
 
         st = Stack(e3 + s0(1))
-
+        
         st.calc()
 
         R = abs(st.R12(0,0))
 
-        R_OK = 0.155960743972
+        R_OK = 0.0362401973292
 
         print R, "expected", R_OK
 
