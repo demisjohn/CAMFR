4932b0e8ffd4ca2320f4a51cc197112d04090146
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2003-08-06 14:10:51 +0000

    New test.

---
+++ trunk/camfr/testsuite/sudbo.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+####################################################################
+#
+# Reproduces 2D waveguide simulations from Sudbo's paper.
+#
+####################################################################
+
+from camfr_work import *
+
+import unittest, eps
+
+class sudbo(unittest.TestCase):
+    def testsudbo(self):
+        
+        """Sudbo"""
+
+        print
+        print "Running Sudbo..."
+
+        set_lambda(1.0)
+        set_N(1)
+
+        core = Material(1.5)
+        clad = Material(1.0)
+
+        set_left_wall(H_wall)
+
+        wg = Slab(core(0.25) + clad(0.76-.25))
+        air = Slab(clad(wg.width()))
+        
+        all = wg(0.25) + air(3.01-.25)
+
+        s = Section(all, 100, 40)
+        s.calc()
+
+        n_eff = s.mode(0).n_eff()
+        n_eff_OK = 1.16243992419
+        
+        print n_eff, "expected", n_eff_OK
+        n_eff_pass = abs((n_eff - n_eff_OK)/n_eff_OK) < eps.testing_eps
+
+        free_tmps()
+
+        self.failUnless(n_eff_pass)
+
+suite = unittest.makeSuite(sudbo, 'test')        
+
+if __name__ == "__main__":
+    unittest.main()
+    
