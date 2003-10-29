78c4e235632e74cff32d2c9b7197260b9020a3ff
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2003-10-29 16:32:17 +0000

    New test.

---
+++ testsuite/degenerate3.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+
+###################################################################
+#
+# Test for uncoupled waveguides.
+#
+###################################################################
+
+from camfr_work import *
+
+import unittest, eps
+
+class degenerate3(unittest.TestCase):
+    def testdegenerate3(self):
+
+        """Degenerate 3"""
+
+        print
+        print "Running degenerate 3..."
+        
+        set_lambda(1.5)
+        set_N(2)
+
+        core=Material(3.2)
+        clad=Material(1.0)
+
+        s1 = Slab(clad(1)+core(0.3)+clad(5.764))
+        s2 = Slab(clad(1)+core(0.3)+clad(4.464)+core(0.3)+clad(1))
+
+        s = Stack(s1(0)+s2(0))
+
+        s.calc()
+
+        R = s.R12(0,0)
+        R_OK = 0.0
+
+        print R, "expected", R_OK
+        R_pass = abs(R - R_OK) < eps.testing_eps
+
+        free_tmps()
+        
+        self.failUnless(R_pass)
+
+suite = unittest.makeSuite(degenerate3, 'test')
+
+if __name__ == "__main__":
+    unittest.main()
+          
