f7cf5fdb510fd321ec6b1b32673243c7021b5782
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2004-06-07 12:24:29 +0000

    Update.

--- trunk/camfr/camfr/__init__.py
+++ trunk/camfr/camfr/__init__.py
@@ -5,11 +5,12 @@ from __future__ import division
 from _camfr import *
 from camfr_PIL import *
 from geometry import *
+from geometry3d import *
 from camfrversion import *
 
 # Splash screen.
 
 print
 print "CAMFR", camfr_version,
-print "- Copyright (C) 1998-2003 Peter Bienstman - Ghent University."
+print "- Copyright (C) 1998-2004 Peter Bienstman - Ghent University."
 print
