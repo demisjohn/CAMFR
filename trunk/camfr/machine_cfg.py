ca72b38631ddd2f2004d0011449a62c8430793b3
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2004-11-24 16:47:13 +0000

    Pieter's new plotting routines.

--- trunk/camfr/machine_cfg.py
+++ trunk/camfr/machine_cfg.py
@@ -41,7 +41,8 @@ library_dirs = ["/opt/intel/mkl/lib/32"]
 
 # Library names.
 
-libs = ["boost_python", "blitz", "mkl_lapack", "mkl_p4", "guide", "g2c"]
+#libs = ["boost_python", "blitz", "mkl_lapack", "mkl_p3", "guide", "g2c"]
+libs = ["boost_python", "blitz", "mkl_lapack", "mkl_def", "guide", "g2c"]
 #libs = ["boost_python", "blitz", "mkl_lapack", "mkl_ia32", "guide", "g2c"]
 #libs = ["boost_python", "blitz", "lapack", "f77blas", "cblas", "atlas", "g2c"]
 
