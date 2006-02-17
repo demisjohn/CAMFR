7e48a5b3d11165503d9d149fbf38c0b43d628b40
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2006-02-17 14:38:42 +0000

    New test.

--- trunk/camfr/testsuite/camfr_test.py
+++ trunk/camfr/testsuite/camfr_test.py
@@ -14,7 +14,7 @@ import blazed_grating, substacks, planarTE, planarTM, VCSEL, SpE, \
        TEM_field, grating2, polariton, gaussian, wg, dent, stack0, stack1, \
        stack2, degenerate2, grating3, sudbo, polariton2, degenerate3, \
        degenerate4, backward, planar_VCSEL, shift, blochstack, w1reson, \
-       surface_plasmon, plasmon_biosensor, backward2, backward3
+       surface_plasmon, plasmon_biosensor, backward2, backward3, slab3
 
 alltests = unittest.TestSuite((blazed_grating.suite, substacks.suite, 
        planarTE.suite, planarTM.suite, VCSEL.suite, SpE.suite, fw_bw.suite,
@@ -25,7 +25,7 @@ alltests = unittest.TestSuite((blazed_grating.suite, substacks.suite,
        dent.suite, stack1.suite, field.suite, slab2.suite, stack2.suite,
        degenerate2.suite, grating3.suite, sudbo.suite, polariton2.suite,
        degenerate3.suite, degenerate4.suite, shift.suite, backward.suite,
-       planar_VCSEL.suite, blochstack.suite, w1reson.suite,
+       planar_VCSEL.suite, blochstack.suite, w1reson.suite, slab3.suite,
        surface_plasmon.suite, plasmon_biosensor.suite, backward2.suite,
        backward3.suite ))
 
