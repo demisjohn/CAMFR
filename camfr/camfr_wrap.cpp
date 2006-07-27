b1a9b77839d11cedd53bc51e4a95c81e40f5f9c8
pbienst <pbienst@78e232d7-6d57-4136-945a-988d5fb676e4>
2006-07-27 14:39:02 +0000

    Split camfr_wrap.cpp.

--- camfr/camfr_wrap.cpp
+++ camfr/camfr_wrap.cpp
@@ -13,6 +13,8 @@
 #include <boost/python.hpp>
 #include "numpy/core/include/numpy/arrayobject.h"
 
+#include "camfr_wrap.h"
+
 #include "defs.h"
 #include "coord.h"
 #include "mode.h"
@@ -26,7 +28,6 @@
 #include "bloch.h"
 #include "icache.h"
 #include "infstack.h"
-#include "math/calculus/function.h"
 #include "primitives/planar/planar.h"
 #include "primitives/circ/circ.h"
 #include "primitives/slab/generalslab.h"
@@ -254,51 +255,12 @@ inline Complex field_H1(const Field& f) {return f.H1;}
 inline Complex field_H2(const Field& f) {return f.H2;}
 inline Complex field_Hz(const Field& f) {return f.Hz;}
 
-inline Complex planar_static_get_kt(Planar p) {return Planar::get_kt();}
-inline void planar_static_set_kt(Planar p, Complex kt) {Planar::set_kt(kt);}
-
-inline void check_index(int i)
-{
-  if ( (i<0) || (i>=int(global.N)) )
-  {
-    PyErr_SetString(PyExc_IndexError, "index out of bounds.");
-    throw std::out_of_range("index out of bounds.");
-  }
-}
-
-inline void check_wg_index(const Waveguide& w, int i)
-{
-  if ( (i<0) || (i>=int(w.N())) )
-  {
-    PyErr_SetString(PyExc_IndexError, "index out of bounds.");
-    throw std::out_of_range("index out of bounds.");
-  }
-}
-
 inline Mode* waveguide_get_mode(const Waveguide& w, int i)
   {check_wg_index(w,i); return w.get_mode(i+1);}
 inline Mode* waveguide_get_fw_mode(const Waveguide& w, int i)
   {check_wg_index(w,i); return w.get_fw_mode(i+1);}
 inline Mode* waveguide_get_bw_mode(const Waveguide& w, int i)
   {check_wg_index(w,i); return w.get_bw_mode(i+1);}
-
-inline SectionMode* section_get_mode(const Section& s, int i)
-{
-  check_wg_index(s,i); 
-  return dynamic_cast<SectionMode*>(s.get_mode(i+1));
-}
-
-inline BlochSectionMode* blochsection_get_mode(const BlochSection& s, int i)
-{
-  check_wg_index(s,i);
-  return dynamic_cast<BlochSectionMode*>(s.get_mode(i+1));
-}
-
-inline BlochMode* blochstack_get_mode(BlochStack& b, int i)
-{
-  check_wg_index(b,i); 
-  return dynamic_cast<BlochMode*>(b.get_mode(i+1));
-}
 
 inline Complex stack_R12(const Stack& s, int i, int j)
   {check_index(i); check_index(j); return s.R12(i+1,j+1);}
@@ -316,13 +278,6 @@ inline Real stack_inc_S_flux(Stack& s, Real c1_start, Real c1_stop, Real eps)
 inline Real stack_ext_S_flux(Stack& s, Real c1_start, Real c1_stop, Real eps)
   {return dynamic_cast<MultiWaveguide*>(s.get_ext())
      ->S_flux(s.ext_field_expansion(),c1_start,c1_stop,eps);}
-
-inline void cavity_set_current_source(Cavity& c, Coord& pos, Coord& ori) 
-  {c.set_source(pos,ori);}
-
-inline void cavity_set_general_source(Cavity& c,
-                                      const cVector& fw, const cVector& bw) 
-  {c.set_source(fw,bw);}
 
 inline boost::python::object stack_fw_bw(Stack& s, Real z)
 {
@@ -344,194 +299,14 @@ inline boost::python::object stack_fw_bw_2(Stack& s, Real z, Limit l)
   return boost::python::make_tuple(fw, bw);
 }
 
-inline boost::python::object blochmode_fw_bw(BlochMode& b, Real z)
-{
-  cVector fw(global.N,fortranArray);
-  cVector bw(global.N,fortranArray);
-
-  b.fw_bw_field(Coord(0,0,z), &fw, &bw);
-
-  return boost::python::make_tuple(fw, bw);
-}
-
-inline boost::python::object blochmode_fw_bw_2(BlochMode& b, Real z, Limit l)
-{
-  cVector fw(global.N,fortranArray);
-  cVector bw(global.N,fortranArray);
-
-  b.fw_bw_field(Coord(0,0,z,Plus,Plus,l), &fw, &bw);
-
-  return boost::python::make_tuple(fw, bw);
-}
-
 inline Real stack_length(Stack& s) 
   {return real(s.get_total_thickness());}
 inline Real stack_width(Stack& s) 
   {return real(s.get_inc()->c1_size());}
-inline Real blochstack_length(BlochStack& bs) 
-  {return real(bs.get_total_thickness());} 
-inline Real blochstack_width(BlochStack& bs)
-  {return real(bs.c1_size());}
-inline Real cavity_length(Cavity& c) 
-  {return real(c.get_bot()->get_total_thickness() 
-             + c.get_top()->get_total_thickness());} 
-inline Real cavity_width(Cavity& c)
-  {return real(c.get_top()->get_inc()->c1_size());}
-inline Real slab_width(Slab& s)
-  {return real(s.get_width());}
-inline Real section_width(Section& s)
-  {return real(s.get_width());}
-inline Real section_height(Section& s)
-  {return real(s.get_height());}
-inline Real blochsection_width(BlochSection& s)
-  {return real(s.get_width());}
-inline Real blochsection_height(BlochSection& s)
-  {return real(s.get_height());}
-inline Complex blochmode_n(BlochMode& m, Coord &c)
-  {return m.get_geom()->n_at(c);}
-inline Complex sectionmode_n(SectionMode& m, Coord &c)
-  {return m.get_geom()->n_at(c);}
 
 inline void free_tmp_interfaces(Waveguide& w)
   {interface_cache.deregister(&w);}
 
-
-
-/////////////////////////////////////////////////////////////////////////////
-//
-// The following functions and classes are used when expanding an
-// abritrarily shaped field in slabmodes.
-//
-/////////////////////////////////////////////////////////////////////////////
-
-/////////////////////////////////////////////////////////////////////////////
-//
-// PythonFunction
-//
-/////////////////////////////////////////////////////////////////////////////
-
-class PythonFunction : public ComplexFunction
-{
-  public:
-
-    PythonFunction(PyObject* f_): f(f_) {}
-
-    Complex operator()(const Complex& z)
-      {counter++; return boost::python::call<Complex>(f, z);}
-
-  protected:
-
-    PyObject* f;
-};
-
-inline cVector slab_expand_field(Slab& s, PyObject* o, Real eps)
-  {PythonFunction f(o); return s.expand_field(&f, eps);}
-
-inline void stack_set_inc_field_function(Stack& s, PyObject* o, Real eps)
-{
-  Slab* slab = dynamic_cast<Slab*>(s.get_inc());
-  
-  if (!slab)
-  {
-    PyErr_SetString(PyExc_ValueError, 
-                    "set_inc_field_function only implemented for slabs.");
-    exit(-1); //throw boost::python::argument_error();
-  }
-
-  PythonFunction f(o);
-  s.set_inc_field(slab->expand_field(&f, eps));
-}
-
-
-
-/////////////////////////////////////////////////////////////////////////////
-//
-// GaussianFunction
-//
-/////////////////////////////////////////////////////////////////////////////
-
-class GaussianFunction : public ComplexFunction
-{
-  public:
-
-    GaussianFunction (Complex height, Complex width, Complex position)
-      : h(height), w(width), p(position) {}
-
-    Complex operator()(const Complex& x)
-	{counter++; return h*exp(-(x-p)*(x-p)/(w*w*2.0));}
-
-  protected:
-
-    Complex h, w, p;
-};
-
-inline cVector slab_expand_gaussian
-  (Slab& s, Complex height, Complex width, Complex pos, Real eps)
-    {GaussianFunction f(height,width,pos); return s.expand_field(&f, eps);}
-
-inline void stack_set_inc_field_gaussian
-  (Stack& s, Complex height, Complex width, Complex pos, Real eps)
-{
-  Slab* slab = dynamic_cast<Slab*>(s.get_inc());
-  
-  if (!slab)
-  {
-    PyErr_SetString(PyExc_ValueError, 
-                    "set_inc_field_gaussian only implemented for slabs.");
-    exit(-1); //throw boost::python::argument_error();
-  }
-
-  GaussianFunction f(height,width,pos);
-  s.set_inc_field(slab->expand_field(&f, eps));
-}
-
-
-
-/////////////////////////////////////////////////////////////////////////////
-//
-// PlaneWaveFunction
-//
-/////////////////////////////////////////////////////////////////////////////
-
-class PlaneWaveFunction : public ComplexFunction
-{
-  public:
-
-    PlaneWaveFunction (Complex amplitude, Complex angle, Complex index)
-      : am(amplitude), an(angle), n(index) {}
-
-    Complex operator()(const Complex& x)
-      {counter++; return am*exp(-2.0*I*pi*sin(an)*n*x/global.lambda);}
-
-  protected:
-
-    Complex am, an, n;
-};
-
-inline cVector slab_expand_plane_wave
-  (Slab& s, const Complex& amplitude, const Complex& angle, Real eps)
-{
-        Complex index = s.get_core()->n();
-        PlaneWaveFunction f(amplitude,angle, index); 
-        return s.expand_field(&f, eps);
-}
-
-inline void stack_set_inc_field_plane_wave
-  (Stack& s, const Complex& amplitude, const Complex& angle, Real eps)
-{
-  Slab* slab = dynamic_cast<Slab*>(s.get_inc());
- 
-  if (!slab)
-  {
-    PyErr_SetString(PyExc_ValueError, 
-                    "set_inc_field_plane_Wave only implemented for slabs.");
-    exit (-1); //throw boost::python::argument_error();
-  }
-
-  Complex index = slab->get_core()->n();
-  PlaneWaveFunction f(amplitude,angle,index);
-  s.set_inc_field(slab->expand_field(&f, eps));
-}
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -650,6 +425,60 @@ Term waveguide_to_term(Waveguide& w, const Complex& d)
 }
 
 
+
+/////////////////////////////////////////////////////////////////////////////
+//
+// The following functions are used when expanding an abritrarily shaped 
+// field in slabmodes.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+inline void stack_set_inc_field_function(Stack& s, PyObject* o, Real eps)
+{
+  Slab* slab = dynamic_cast<Slab*>(s.get_inc());
+  
+  if (!slab)
+  {
+    py_error("set_inc_field_function only implemented for slabs.");
+    exit(-1);
+  }
+
+  PythonFunction f(o);
+  s.set_inc_field(slab->expand_field(&f, eps));
+}
+
+inline void stack_set_inc_field_gaussian
+  (Stack& s, Complex height, Complex width, Complex pos, Real eps)
+{
+  Slab* slab = dynamic_cast<Slab*>(s.get_inc());
+  
+  if (!slab)
+  {
+    py_error("set_inc_field_gaussian only implemented for slabs.");
+    exit(-1);
+  }
+
+  GaussianFunction f(height,width,pos);
+  s.set_inc_field(slab->expand_field(&f, eps));
+}
+
+inline void stack_set_inc_field_plane_wave
+  (Stack& s, const Complex& amplitude, const Complex& angle, Real eps)
+{
+  Slab* slab = dynamic_cast<Slab*>(s.get_inc());
+ 
+  if (!slab)
+  {
+    py_error("set_inc_field_plane_Wave only implemented for slabs.");
+    exit (-1);
+  }
+
+  Complex index = slab->get_core()->n();
+  PlaneWaveFunction f(amplitude,angle,index);
+  s.set_inc_field(slab->expand_field(&f, eps));
+}
+
+
   
 /////////////////////////////////////////////////////////////////////////////
 //
@@ -669,9 +498,6 @@ void stack_set_inc_field(Stack& s, const cVector& f)
 void stack_set_inc_field_2(Stack& s, const cVector& f, const cVector& b) 
   {s.set_inc_field(f, &const_cast<cVector&>(b));}
 
-Real cavity_calc_sigma(Cavity& c)
-  {return c.calc_sigma();}
-
 Complex material_epsr(Material& m)
   {return m.epsr();}
 
@@ -684,11 +510,6 @@ Complex material_mur(Material& m)
 Complex basematerial_mur(Material& m, int i)
   {return m.mur(i);}
 
-BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(cav_find_mode, Cavity::find_mode,2,5)
-
-BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(cav_find_modes, \
-  Cavity::find_modes_in_region,3,7)
-
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -699,6 +520,8 @@ BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(cav_find_modes, \
 
 #undef PyArray_Type
 extern PyTypeObject PyArray_Type;
+
+void camfr_wrap_2();
 
 BOOST_PYTHON_MODULE(_camfr)
 {
@@ -1172,194 +995,8 @@ BOOST_PYTHON_MODULE(_camfr)
     .def(self + Term())
     ;
 
-  // Wrap Cavity.
+  // The rest of the wrappers.
 
-  class_<Cavity>("Cavity", init<Stack&, Stack&>())
-    .def("find_mode",      &Cavity::find_mode, cav_find_mode())
-    .def("find_all_modes", &Cavity::find_modes_in_region,cav_find_modes())
-    .def("sigma",          cavity_calc_sigma)
-    .def("set_source",     cavity_set_current_source)
-    .def("set_source",     cavity_set_general_source)
-    .def("length",         cavity_length)
-    .def("width",          cavity_width)
-    .def("field",          &Cavity::field)
-    .def("n",              &Cavity::n_at)
-    .def("bot_stack",      &Cavity::get_bot,
-         return_value_policy<reference_existing_object>())
-    .def("top_stack",      &Cavity::get_top,
-         return_value_policy<reference_existing_object>())
-    ;
-
-  // Wrap BlochStack.
-
-  class_<BlochStack, bases<MultiWaveguide> >
-    ("BlochStack", init<const Expression&>())
-    .def("mode",        blochstack_get_mode,
-         return_value_policy<reference_existing_object>())
-    .def("length",      blochstack_length)
-    .def("width",       blochstack_width)
-    .def("beta_vector", &BlochStack::get_beta_vector)
-    .def("__repr__",    &BlochStack::repr)
-    ;
-
-  // Wrap BlochMode.
-
-  class_<BlochMode, bases<Mode> >("BlochMode", no_init)
-    .def("fw_field", &BlochMode::fw_field)
-    .def("bw_field", &BlochMode::bw_field)
-    .def("fw_bw",    blochmode_fw_bw)
-    .def("fw_bw",    blochmode_fw_bw_2)
-    .def("S_flux",   &BlochMode::S_flux)
-    .def("n",        blochmode_n)
-    ;
-
-  // Wrap InfStack.
-
-  class_<InfStack, bases<DenseScatterer> >
-    ("InfStack", init<const Expression&>())
-    .def("R12", &InfStack::get_R12,
-         return_value_policy<reference_existing_object>())
-    ;
-
-  // Wrap RealFunction.
-
-  class_<RealFunction, boost::noncopyable>("RealFunction", no_init)
-    .def("times_called", &RealFunction::times_called)
-    .def("__call__",     &RealFunction::operator())
-    ;
-
-  // Wrap ComplexFunction.
-
-  class_<ComplexFunction, boost::noncopyable>("RealFunction", no_init)
-    .def("times_called", &ComplexFunction::times_called)
-    .def("__call__",     &ComplexFunction::operator())
-    ;
-
-  // Wrap Planar.
-
-  class_<Planar, bases<MonoWaveguide> >("Planar", init<Material&>())
-    .def("set_theta", &Planar::set_theta)    
-    .def("set_kt",    planar_static_set_kt)
-    .def("get_kt",    planar_static_get_kt)
-    ;
-
-  // Wrap Circ.
-
-  class_<Circ, bases<MultiWaveguide> >("Circ", init<Term&>())
-    .def(init<Expression&>())
-    ;
-
-  // Wrap SlabWall.
-
-  class_<SlabWall, boost::noncopyable>("SlabWall", no_init)
-    .def("R", &SlabWall::get_R12)
-    ;
-
-  // Wrap SlabWallMixed.
-
-  class_<SlabWallMixed, bases<SlabWall> >
-    ("SlabWallMixed", init<const Complex&, const Complex&>());
-
-  scope().attr("slab_E_wall")  = SlabWallMixed(1.0,  1.0);
-  scope().attr("slab_H_wall")  = SlabWallMixed(1.0, -1.0);
-  scope().attr("slab_no_wall") = SlabWallMixed(0.0,  1.0);
-
-  // Wrap SlabWall_TBC.
-
-  class_<SlabWall_TBC, bases<SlabWall> >
-    ("SlabWall_TBC", init<const Complex&, const Material&>());
-
-  // Wrap SlabWall_PC.
-
-  class_<SlabWall_PC, bases<SlabWall> >
-    ("SlabWall_PC", init<const Expression&>());
-
-  // Wrap SlabDisp.
-
-  class_<SlabDisp, bases<ComplexFunction> >
-    ("SlabDisp", init<Expression&, Real>())
-    .def(init<Expression&, Real, SlabWall*, SlabWall*>())
-    ;
-
-  // Wrap Slab.
-
-  class_<Slab, bases<MultiWaveguide> >("Slab", init<const Term&>())
-    .def(init<const Expression&>())
-    .def("set_lower_wall",    &Slab::set_lower_wall)
-    .def("set_upper_wall",    &Slab::set_upper_wall)
-    .def("width",             slab_width)
-    //.def("disp",              &Slab::get_disp)
-    .def("expand_field",      slab_expand_field)
-    .def("expand_gaussian",   slab_expand_gaussian) 
-    .def("expand_plane_wave", slab_expand_plane_wave)
-    .def("set_dummy",         &Slab::set_dummy)    
-    .def("add_kz2_estimate",  &Slab::add_kz2_estimate)
-    ;
-
-  // Wrap SectionDisp.
-
-  class_<SectionDisp, bases<ComplexFunction> >
-    ("SectionDisp", init<Stack&, Stack&, Real, int>());
-
-  // Wrap Section.
-
-  class_<Section, bases<MultiWaveguide> >
-  ("Section", init<Expression&, optional<int, int> >())
-    .def(init<Expression&, Expression&, optional<int, int> >())    
-    .def(init<const Term&>())
-    .def("mode",         section_get_mode,
-         return_value_policy<reference_existing_object>())
-    .def("disp",         &Section::get_disp)
-    .def("width",        section_width)
-    .def("height",       section_height)
-    .def("eps",          &Section::eps_at)
-    .def("mu",           &Section::mu_at)
-    .def("n",            &Section::n_at)
-    .def("set_sorting",  &Section::set_sorting)
-    .def("set_estimate", &Section::set_estimate)
-    ;
-
-  // Wrap RefSection.
-
-  class_<RefSection, bases<MultiWaveguide> >
-  ("RefSection", init<Material&, const Complex&, const Complex&, int>())
-    ;  
-
-  // Wrap SectionMode.
-
-  class_<SectionMode, boost::noncopyable, bases<Mode> >
-    ("SectionMode", no_init)
-    .def("n", sectionmode_n)
-    ;
-
-  // Wrap BlochSection.
-
-  class_<BlochSection, bases<MultiWaveguide> >
-  ("BlochSection", init<Expression& >())    
-    .def(init<const Term&>())
-    .def("mode",          blochsection_get_mode,
-         return_value_policy<reference_existing_object>())
-    .def("width",         blochsection_width)
-    .def("height",        blochsection_height)
-    .def("eps",           &BlochSection::eps_at)
-    .def("mu",            &BlochSection::mu_at)
-    .def("n",             &BlochSection::n_at)
-    .def("order",         &BlochSection::order)
-    .def("set_theta_phi", &BlochSection::set_theta_phi)    
-    .def("set_kx0_ky0",   &BlochSection::set_kx0_ky0)    
-    .def("get_kx0",       &BlochSection::get_kx0)    
-    .def("get_ky0",       &BlochSection::get_ky0)
-    ;
-
-  // Wrap BlochSectionMode.
-
-  class_<BlochSectionMode, boost::noncopyable, bases<Mode> >
-    ("BlochSectionMode", no_init)
-    .def("get_Mx",   &BlochSectionMode::get_Mx)    
-    .def("get_My",   &BlochSectionMode::get_My)    
-    .def("get_kx",   &BlochSectionMode::get_kx)    
-    .def("get_ky",   &BlochSectionMode::get_ky)
-    ;
-
+  camfr_wrap_2();
 }
 
