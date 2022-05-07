fix test units

testsuite/metal_coupler.py
@@ -80,7 +80,7 @@ class metal_coupler(unittest.TestCase):
 
         guided  = 0
         campmax = 0
-        for k in np.arange(0,nom,1):
+        for k in range(0,nom,1):
             camp = abs(waveguide.mode(k).field(Coord(d_sub+dclad+\
                                             guide_thickness/2,0,0)).E1())
             if campmax < camp:
@@ -89,7 +89,7 @@ class metal_coupler(unittest.TestCase):
 
         # Set input for calculating the fields.
 
-        inc = zeros(N())
+        inc = np.zeros(N())
         inc[guided] = 1
         stack.set_inc_field(inc)
 

testsuite/stack0.py
@@ -30,7 +30,7 @@ class stack0(unittest.TestCase):
         
         s = Stack(AlGaAs(0) + air(1) + AlGaAs(0))
 
-        inc = zeros(N())
+        inc = np.zeros(N())
         inc[0] = 1
         s.set_inc_field(inc)
 